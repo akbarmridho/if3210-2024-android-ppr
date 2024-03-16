5f86c075afc68de1a71b30c06aba623398da18d1
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-03-16 18:09:06 +0700

    feat: setup api client for http request

---
+++ app/src/main/java/com/informatika/bondoman/api/ApiClient.kt
@@ -0,0 +1,26 @@
+package com.informatika.bondoman.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://pbd-backend-2024.vercel.app/api/"
+    private val client = OkHttpClient.Builder()
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+}
