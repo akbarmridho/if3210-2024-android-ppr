3ebda04d647f7a822f984dc2f21c953a6bb26980
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-03-21 22:20:53 +0700

    refactor: api client add interceptor

--- app/src/main/java/com/informatika/bondoman/network/ApiClient.kt
+++ app/src/main/java/com/informatika/bondoman/network/ApiClient.kt
@@ -1,15 +1,21 @@
 package com.informatika.bondoman.network
 
+import com.informatika.bondoman.BuildConfig
 import com.informatika.bondoman.model.remote.AuthService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://pbd-backend-2024.vercel.app/api/"
+    private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
     private val client = OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
         .build()
 
     private val moshi = Moshi.Builder()
@@ -18,7 +24,7 @@ object RetrofitClient {
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .client(client)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
