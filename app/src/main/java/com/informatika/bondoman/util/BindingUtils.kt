a16636ae053fa8f17e406cfca27a985269aee68a
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-03-23 00:40:11 +0700

    add some functionality for binding data

---
+++ app/src/main/java/com/informatika/bondoman/util/BindingUtils.kt
@@ -0,0 +1,43 @@
+package com.informatika.bondoman.util
+
+import android.icu.text.DecimalFormat
+import android.icu.text.NumberFormat
+import com.informatika.bondoman.model.local.entity.transaction.Category
+
+object BindingUtils {
+    @JvmStatic
+    fun createTitle(title: String): String {
+        // if the title is too long, stop at 20 characters and add "..."
+        return if (title.length > 20) {
+            title.substring(0, 20) + "..."
+        } else {
+            title
+        }
+    }
+
+    @JvmStatic
+    fun getCategory(category: Category): String {
+        return when (category) {
+            Category.INCOME -> "Income"
+            Category.EXPENSE -> "Expense"
+            Category.LOADER -> ""
+        }
+    }
+
+    @JvmStatic
+    fun createAmount(amount: Int): String {
+        val formatter: NumberFormat = DecimalFormat("#,###")
+        val formattedNumber: String = formatter.format(amount)
+        return "$formattedNumber"
+    }
+
+    @JvmStatic
+    fun isLocationNotNull(location: String?): Boolean {
+        return location != null
+    }
+
+    @JvmStatic
+    fun getDate(s: String): String? {
+        return s.split("T")[0]
+    }
+}
