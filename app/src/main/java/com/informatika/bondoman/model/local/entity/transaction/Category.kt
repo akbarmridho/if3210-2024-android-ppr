dfeefc7f20dd31e8055d33cc1762762c920538c6
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-03-23 00:40:52 +0700

    refactor: add category for loading

--- app/src/main/java/com/informatika/bondoman/model/local/entity/transaction/Category.kt
+++ app/src/main/java/com/informatika/bondoman/model/local/entity/transaction/Category.kt
@@ -1,6 +1,7 @@
 package com.informatika.bondoman.model.local.entity.transaction
 
 enum class Category(s: String) {
-    PEMASUKAN("Pemasukan"),
-    PENGELUARAN("Pengeluaran")
+    INCOME("Income"),
+    EXPENSE("Expense"),
+    LOADER("")
 }
