73c80a0d748d6a6af341a571859d6a8b2bb0c183
Fatih20 <fnri39@protonmail.com>
2024-04-01 13:08:36 +0700

    feat: initial work on connecting to the report view model

--- app/src/main/java/com/informatika/bondoman/view/fragment/ReportFragment.kt
+++ app/src/main/java/com/informatika/bondoman/view/fragment/ReportFragment.kt
@@ -6,21 +6,66 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.informatika.bondoman.R
+import com.informatika.bondoman.databinding.FragmentReportBinding
+import com.informatika.bondoman.model.Resource
+import com.informatika.bondoman.model.local.entity.transaction.CategoryPercentage
 import com.informatika.bondoman.viewmodel.ReportViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Locale.Category
 
 class ReportFragment : Fragment() {
+
+    lateinit var mReportFragmentBinding: FragmentReportBinding
+    private val reportViewModel: ReportViewModel by viewModels()
 
     companion object {
         fun newInstance() = ReportFragment()
     }
 
-    private val viewModel: ReportViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_report, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        reportViewModel.transactionAggregation.observe(viewLifecycleOwner, Observer { resource ->
+            resource?.let {
+                when (it) {
+                    is Resource.Success -> {
+                        displayChart(it.data)
+                    }
+                    is Resource.Error ->
+                        displayErrorText()
+                    is Resource.Loading ->
+                        displayLoadingText()
+                }
+            }
+        })
+
+        reportViewModel.getTransactionAggregation();
+    }
+
+    fun displayErrorText() {
+        mReportFragmentBinding.chartWrapper.visibility = View.GONE
+        mReportFragmentBinding.reportLoadingText.visibility = View.GONE
+        mReportFragmentBinding.reportErrorText.visibility = View.VISIBLE
+    }
+
+    fun displayLoadingText() {
+        mReportFragmentBinding.chartWrapper.visibility = View.VISIBLE
+        mReportFragmentBinding.reportLoadingText.visibility = View.VISIBLE
+        mReportFragmentBinding.reportErrorText.visibility = View.GONE
+    }
+
+    fun displayChart(data : List<CategoryPercentage>) {
+        mReportFragmentBinding.chartWrapper.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/informatika/bondoman/viewmodel/ReportViewModel.kt
+++ app/src/main/java/com/informatika/bondoman/viewmodel/ReportViewModel.kt
@@ -1,7 +1,33 @@
 package com.informatika.bondoman.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.informatika.bondoman.model.Resource
+import com.informatika.bondoman.model.local.entity.transaction.CategoryPercentage
+import com.informatika.bondoman.model.repository.transaction.TransactionRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ReportViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ReportViewModel(private var transactionRepository: TransactionRepository) : ViewModel() {
+    val transactionAggregation = MutableLiveData<Resource<List<CategoryPercentage>>>()
+    private val observer = Observer<Resource<List<CategoryPercentage>>> {
+        transactionAggregation.postValue(it)
+    }
+
+    init {
+        transactionRepository.categoryPercentageLiveData.observeForever(observer)
+    }
+
+    fun getTransactionAggregation() {
+        viewModelScope.launch(Dispatchers.IO) {
+            transactionRepository.getCategoryPercentage()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        transactionRepository.categoryPercentageLiveData.removeObserver(observer)
+    }
 }
