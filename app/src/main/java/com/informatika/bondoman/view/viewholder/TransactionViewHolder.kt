3aef1dc33848f6b3ecb332c8c0d2dc0b72c06758
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-04-04 23:22:34 +0700

    add open transaction location

--- app/src/main/java/com/informatika/bondoman/view/viewholder/TransactionViewHolder.kt
+++ app/src/main/java/com/informatika/bondoman/view/viewholder/TransactionViewHolder.kt
@@ -1,6 +1,8 @@
 package com.informatika.bondoman.view.viewholder
 
 import android.view.View
+import android.content.Intent
+import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
 import com.informatika.bondoman.databinding.ItemTransactionBinding
 import com.informatika.bondoman.model.local.entity.transaction.Transaction
@@ -9,6 +11,15 @@ class TransactionViewHolder(private var mTransactionBinding: ItemTransactionBind
     RecyclerView.ViewHolder(mTransactionBinding.root) {
     fun setTransaction(transaction: Transaction) {
         mTransactionBinding.transaction = transaction
+
+        mTransactionBinding.tvListItemTransactionLocation.setOnClickListener{
+            if (transaction.location != null) {
+                val gmmIntentUri = "http://maps.google.com/maps?q=loc:${transaction.location.lat},${transaction.location.lon}"
+                val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(gmmIntentUri))
+                mapIntent.setPackage("com.google.android.apps.maps")
+                mTransactionBinding.root.context.startActivity(mapIntent)
+            }
+        }
     }
 
     fun setClickListener(clickListener: View.OnClickListener) {
